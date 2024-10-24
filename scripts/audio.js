9deb212166bd51cc9c064537e757f0befda16063
xooose <xooose9@gmail.com>
2024-10-25 01:11:28 +0700

    Add omg images

--- scripts/audio.js
+++ scripts/audio.js
@@ -6,8 +6,28 @@ function playAudio() {
     });
 }
 
-// Play the song when overlay is gone.
+function showImagesSequentially() {
+    const image1 = document.getElementById('image1');
+    const image2 = document.getElementById('image2');
+    const image3 = document.getElementById('image3');
+
+    console.log(image1.style.display)
+    image1.style.display = 'block';
+    setTimeout(() => {
+        image1.style.display = 'none';
+        image2.style.display = 'block';
+        setTimeout(() => {
+            image2.style.display = 'none';
+            image3.style.display = 'block';
+            setTimeout(() => {
+                image3.classList.add('fade-out', 'hidden');
+            }, 1200);
+        }, 300);
+    }, 300);
+}
+
 document.getElementById('overlay').addEventListener('click', function() {
     document.getElementById('overlay').style.display = 'none';
     playAudio();
+    showImagesSequentially();
 });
